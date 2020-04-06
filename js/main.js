6694b3c2eeb200bcbbef7722cfb0542e80ed4265
Phillip Paul <phillippaul@gmx.de>
2020-04-06 12:35:34 +0200

    Initial blank folder structure: html, css, js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// pre-set variables
+
+//
+function preload() {
+  console.log("preload");
+}
+
+function setup() {
+  // setup happens after preload
+  // setup everything you want to happen before the draw
+  console.log("setup");
+}
+
+/*
+function draw() {
+  console.log("draw");
+}
+*/
